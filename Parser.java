cc16e2c253d213e15ff62aa7bbd217f707ca4465
Bernard Sexton <b.sexton@warwick.ac.uk>
2013-03-23 23:23:47 +0000

    now parses xml files that are clean. run by specifying the last 2 digits of the relevant xml name eg java Parser 02

--- Parser.java
+++ Parser.java
@@ -1,13 +1,18 @@
-import java.io.*;
-import java.util.*;
-//keeping dom parser, could use sax in future
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.DocumentBuilder;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.w3c.dom.Node;
-import org.w3c.dom.Element;
+import javax.xml.parsers.FactoryConfigurationError;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Parser {
 
@@ -28,21 +33,135 @@ public class Parser {
 		
 		//file path for reuters files
 		dirpath = "reuters21578/reut2-0";
-		//number of sgm files
-		sgmCount = 21;
+
 		//call file load
-		loadSGMFile("01");
+		parseXMLFile(args[0]);
+	}
+	
+	public static void parseXMLFile(String filenum) {
+	
+		//Loads the requested XML file and splits out the desired tags that we are
+		//interested in. This will eventually go to a tokenizer and is stored as
+		//in a document object.
+		
+		ReutersDocument document = new ReutersDocument();
+		
+		List<ReutersDocument> documentCollection = new ArrayList<ReutersDocument>();
+		
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		
+		//SAX Handler callback functions.
+		DefaultHandler handler = new DefaultHandler() {
+		
+			boolean title = false;
+			boolean dateline = false;
+			boolean body = false;
+			boolean endofdoc = false;
+		
+			public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+ 
+				if (qName.equalsIgnoreCase("title")) {
+					title = true;
+				}
+ 
+				if (qName.equalsIgnoreCase("dateline")) {
+					dateline = true;
+				}
+ 
+				if (qName.equalsIgnoreCase("body")) {
+					body = true;
+				}
+ 
+			}	
+			
+			public void characters(char ch[], int start, int length) throws SAXException {
+ 
+				if (title) {
+					System.out.println("Document title : " + new String(ch, start, length));
+					title = false;
+				}
+ 
+				if (dateline) {
+					System.out.println("Document date : " + new String(ch, start, length));
+					dateline = false;
+				}
+ 
+				if (body) {
+					System.out.println("Document body : " + new String(ch, start, length));
+					body = false;
+				}
+ 
+			}
+			
+			public void endElement(String uri, String localName, String qName) throws SAXException {
+ 
+				if (qName.equalsIgnoreCase("reuters")) {
+					endofdoc = true;
+				}
+ 
+			}
+		
+		};
+	
+		try {
+	
+			SAXParser parser = factory.newSAXParser();
+			parser.parse(dirpath+filenum+".xml", handler);
+			System.out.println("(PARSER): Parsing complete");
+		
+		} catch (Exception e) {
+		
+			System.out.println(e.toString());
+		
+		}
+	
+	
 	}
 	
 	public static void loadSGMFile(String sgmNumber) {
 	
-		try {
+		//Loads the requested SGM file, removing noise and converting into
+		//a well formed XML file that exists in the reuters21578 subdirectory.
+		//Essentially a deprecated method now that we have clean xml files.
+		
+		SAXParserFactory factory = SAXParserFactory.newInstance();
+		factory.setNamespaceAware(true);
+		DefaultHandler handler = new DefaultHandler();
 	
+		try {
+			
+			//1) First add a root node as file isn't well formed.
+			String ln;
+		    SAXParser parser = factory.newSAXParser();
+			
+			//a)Temp file
+			File file = new File(dirpath+sgmNumber+".xml");
+			file.createNewFile();
+			
+			//b)Load sgm file
 			File sgmFile = new File(dirpath+sgmNumber+".sgm");
-			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(sgmFile);
-			System.out.println("Document " + sgmNumber + " loaded");
+			BufferedReader br = new BufferedReader(new FileReader(dirpath+sgmNumber+".sgm"));
+			
+			//c)Write to file
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write("<ReutersRoot>");
+			
+			while ((ln = br.readLine()) != null) {
+				if(!ln.contains("DOCTYPE")) {
+					bw.write(ln.replaceAll("&#","")+"\n");
+				} 
+			}
+			
+			bw.write("</ReutersRoot>");
+		
+			bw.flush();
+			bw.close();
+			
+			parser.parse(dirpath+sgmNumber+".xml", handler);
+			System.out.println("(PARSER): Parsing complete");
+			
 		
 		} catch (Exception e) {
 			
--- ReutersDocument.java
+++ ReutersDocument.java
@@ -15,17 +15,114 @@ public class ReutersDocument {
 
 	public ReutersDocument (String topics, String lewissplit, String cgisplit, String oldid, String newid, String title, String dateline, String text) {
 
-		TOPICS = topics;
-		LEWISSPLIT = lewissplit;
-		CGISPLIT = cgisplit;
-		OLDID = oldid;
-		NEWID = newid;
-		TITLE = title;
-		DATELINE = dateline;
-		TEXT = text;
+		this.TOPICS = topics;
+		this.LEWISSPLIT = lewissplit;
+		this.CGISPLIT = cgisplit;
+		this.OLDID = oldid;
+		this.NEWID = newid;
+		this.TITLE = title;
+		this.DATELINE = dateline;
+		this.TEXT = text;
 
 	}
+	
+	public ReutersDocument() {}
+	
+	public void setTopics(String topics) {
+	
+		TOPICS = topics;
+		
+	}
+	
+	public void setLewis(String lewis) {
+	
+		LEWISSPLIT = lewis;
+	
+	}
+	
+	public void setCgi(String cgi) {
+	
+		CGISPLIT = cgi;
+	
+	}
+	
+	public void setoldid(String old) {
+	
+		OLDID = old;
+	
+	}
 
+	public void setnewid(String newid) {
+	
+		NEWID = newid;
+	
+	}
+	
+	public void setTitle(String title) {
+	
+		TITLE = title;
+	
+	}
+	
+	public void setDateline(String dateline) {
+	
+		DATELINE = dateline;
+	
+	}
+	
+	public void setText(String text) {
+	
+		TEXT = text;
+	
+	}
+	
+	public String getTopics() {
+	
+		return TEXT;
+	
+	}
+	
+	public String getLewis() {
+	
+		return LEWISSPLIT;
+	
+	}
+	
+	public String getCgi() {
+	
+		return CGISPLIT;
+	
+	}
+	
+	public String getoldid() {
+	
+		return OLDID;
+	
+	}
+
+	public String getnewid() {
+	
+		return NEWID;
+	
+	}
+	
+	public String getTitle() {
+	
+		return TITLE;
+	
+	}
+	
+	public String getDateline() {
+	
+		return DATELINE;
+	
+	}
+	
+	public String getText() {
+	
+		return TEXT;
+	
+	}
 }
 
 

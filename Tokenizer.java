8014d6acb2b96adbdf75838a63e4bebebafbb67d
Bernard Sexton <b.sexton@warwick.ac.uk>
2013-03-30 15:50:48 +0000

    removing stop words

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,8 +1,16 @@
 import java.io.IOException;
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
 import java.io.StringReader;
 import java.io.StreamTokenizer;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileWriter;
 
 public class Tokenizer {
 
@@ -19,10 +27,14 @@ public class Tokenizer {
 	private List<ReutersDocument> testSet;
 	private List<ReutersDocument> trainingSet;
 	private StreamTokenizer	tokenizerObject;
+	private File stopwords;
+	private Hashtable<String, String> stopSet;
+	private BufferedReader bReader;
 	
 	public Tokenizer(List<ReutersDocument> documentSet) {
 	
 		this.documentSet = documentSet;
+		stopSet = new Hashtable<String,String>();
 	
 	}
 	
@@ -33,8 +45,40 @@ public class Tokenizer {
 		String dateLine;
 		String body;
 		String[] tokenArray;
+		String tmpLine = "";
 		List<String> tokenList = new ArrayList<String>();
-	
+		
+		//pull in stopwords and place in hash table to be used later.
+		try { 
+		
+			stopwords = new File("reuters21578/stopwords.txt");
+			bReader = new BufferedReader(new FileReader(stopwords));
+			
+			while((tmpLine = bReader.readLine())!=null) {
+				
+				String[] dataArray = tmpLine.split(",");
+						
+				for (String item:dataArray) { 
+					stopSet.put(item,item);
+				}
+			
+			}
+			
+			bReader.close();
+			
+			
+		} catch (FileNotFoundException err) {
+		
+			System.out.println(err.toString());
+			
+		} catch (IOException err) {
+		
+			System.out.println(err.toString());
+		
+		}
+		
+		//proceed to tokenize each document
+		
 		for (ReutersDocument document : documentSet) {
 			
 			//get body text from current document
@@ -44,12 +88,17 @@ public class Tokenizer {
 					
 				//enter text into a tokenizer object
 				tokenizerObject = new StreamTokenizer(new StringReader(body));
-			
+				
 				while(tokenizerObject.nextToken() != StreamTokenizer.TT_EOF){
-
+				
 					if(tokenizerObject.ttype == StreamTokenizer.TT_WORD) {
-						System.out.println(tokenizerObject.sval);
-						tokenList.add(tokenizerObject.sval);
+					
+						//normalize case of words.
+						if(!isStopWord(tokenizerObject.sval.toLowerCase())) {
+							System.out.println(tokenizerObject.sval.toLowerCase());						
+							tokenList.add(tokenizerObject.sval.toLowerCase());
+						}
+						
 					} else if(tokenizerObject.ttype == StreamTokenizer.TT_EOL) {
 						System.out.println();	
 					}
@@ -68,6 +117,19 @@ public class Tokenizer {
 		}
 	
 		return documentSet;
+		
+	}
+	
+	public boolean isStopWord(String word) {
+	
+		//checks existence in stopwords hashmap.
+		
+		if (stopSet.containsKey(word)) {
+			System.out.println("Removed stop word: " + word); 
+			return true;
+		} else {
+			return false;
+		}
 		
 	}
 

a19a1339a31d10fb164420500b5eded94d255e6e
Janssens <robin.janssens@gmail.com>
2013-04-14 20:57:25 +0100

    some improvements to model

---
+++ MDM.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+import weka.core.Instances;
+import weka.core.converters.TextDirectoryLoader;
+import weka.filters.Filter;
+import weka.filters.unsupervised.attribute.StringToWordVector;
+import weka.classifiers.bayes.NaiveBayesMultinomial;
+
+public class MDM implements CustomModel{
+	
+	//this can probs be abstracted out at some point... 
+	private StringToWordVector swv; 
+	
+	private NaiveBayesMultinomial nbm;
+	
+	public MDM (){
+	
+		this.swv = new StringToWordVector();  
+		this.nbm = new NaiveBayesMultinomial();
+	}
+	
+	public void runModel() {
+		
+		String swvOptions; 
+		String nbmOptions; 
+	}	
+
+}
